07db5486bf95b22969f2e34e4e2d7bde07d4cac2
Kelsey Jungermann <kelseyjungermann@Kelseys-MBP.lan>
2023-03-11 20:05:15 -0600

    initial commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
-
+  photosTitle = 'Some Web Dev Funnies';
+  image1 = 'https://i.redd.it/fqwwrmw3iav61.jpg';
+  image2 = 'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjAzNTdkMTQ0YTA0MjQ5NmE2MDkxMDY0NjU0ZjE0MzIzZDBjZDExMSZjdD1n/XBEoaajXTXaALzawSn/giphy.gif';
+  image3 = 'https://cdn.dribbble.com/users/6056081/screenshots/17758417/media/d713673f10be2404732955e7714db120.jpg?compress=1&resize=400x300';
+  image4 = 'https://img.devrant.com/devrant/rant/r_1936979_wQRxc.jpg'
   constructor() { }
 
   ngOnInit() {
